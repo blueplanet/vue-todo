55b31ec9d37c1f07acf013e496af793a39fd4b70
blueplanet <erguolinge@gmail.com>
2017-04-25 07:31:26 +0900

    add todo

--- js/app.js
+++ js/app.js
@@ -2,6 +2,21 @@ const app = new Vue({
   data: {
     newTodo: '',
     todos: []
+  },
+  methods: {
+    addTodo: function() {
+      let value = this.newTodo && this.newTodo.trim();
+      if (!value) {
+        return
+      }
+
+      this.todos.push({
+        title: value,
+        compeleted: false
+      })
+
+      this.newTodo = ''
+    }
   }
 });
 
